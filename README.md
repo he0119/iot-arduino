34547e3c706f361a90b38b6a446b9510ba48c037
hemengyang <hmy0119@gmail.com>
2019-08-14 17:35:45 +0800

    Update docs (#1)
    * Code cleanup
    * Update dependencies
    * Add english comments

--- README.md
+++ README.md
@@ -4,11 +4,9 @@ This is a submodule of iot project.
 
 ## Requirements
 
-### ESP8266
-
 - `ESP8266 core`: 2.4.2
 - `NTPClient`: 3.1.0
-- `ArduinoJson`: 6.10.0
+- `ArduinoJson`: 6.11.4
 - `SocketIoClient`: 0.3
-- `WebSockets`: 2.1.1
+- `WebSockets`: 2.1.4
 - `DHTstable`: 0.2.4
--- esp8266/autowatering/autowatering.ino
+++ esp8266/autowatering/autowatering.ino
@@ -1,36 +1,47 @@
-//WIFI&OTA&FS
+// Pins
+#define DHT_PIN 2            // D4
+#define PUMP_PIN 5           // D1
+#define VALVE_PIN 4          // D2
+#define SOIL_MOISTURE_PIN 17 // A0
+
+// Config
+#include "config.h"
+#ifdef ENABLE_DEBUG
+#define DEBUG_PRINTLN(...) Serial.println(__VA_ARGS__);
+#else
+#define DEBUG_PRINTLN(...)
+#endif
+
+// WIFI&OTA&FS
 #include <ESP8266WiFi.h>
 #include <ArduinoOTA.h>
 #include <FS.h>
 
-//Ticker&Watchdog
+// Ticker&Watchdog
 #include <Ticker.h>
 Ticker secondTick;
 volatile int watchdogCount = 1;
 
-//Config
-#include "config.h"
-
-//NTP
+// NTP
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "ntp.aliyun.com", 0, 60000);
-//FIXME:莫名奇妙会无法与NTP服务器同步时间。(猜测是和路由器有关)
+NTPClient timeClient(ntpUDP, "ntp.aliyun.com");
+// FIXME: Sometime it can't connect to ntp server, may causeed by router.
 
-//Json
+// Json
 #include <ArduinoJson.h>
 
-//Socket
+// SocketIO
 #include <SocketIoClient.h>
 #ifdef ENABLE_SSL
 #define beginwebsocket beginSSL
 #else
 #define beginwebsocket begin
 #endif
-SocketIoClient webSocket;
+SocketIoClient socketIO;
 
-//DHT
+// DHT
 #include <dht.h>
 #ifdef DHT_VERSION_11
 #define readdht read11
@@ -38,45 +49,40 @@ SocketIoClient webSocket;
 #ifdef DHT_VERSION_22
 #define readdht read22
 #endif
-#define DHT_PIN D4 //连接到DHT传感器的端口
 dht DHT;
 
-//Status
-unsigned long lastMillis = 0; //计时器
+// Status
+unsigned long lastMillis = 0; // Upload Data Timer
 float temperature;
 float relative_humidity;
 unsigned long data_readtime;
 
-//Pump&Valve
-unsigned long valveMillis = 0; //电磁阀自动关闭计时器
-bool valve_delay_trigger = 0;  //电磁阀自动关闭触发器
-unsigned long pumpMillis = 0;  //抽水机自动关闭计时器
-bool pump_delay_trigger = 0;   //抽水机自动关闭触发器
+// Pump&Valve
+unsigned long valveMillis = 0; // Valve Auto Close Timer
+bool valve_auto_close = false; // Valve Auto Close Switch
+unsigned long pumpMillis = 0;  // Pump Auto Close Timer
+bool pump_auto_close = false;  // Pump Auto Close Switch
 bool valve = false;
 bool pump = false;
-unsigned long valve_delay = 60; //电磁阀延时，单位 秒
-unsigned long pump_delay = 60;  //抽水机延时，单位 秒
+unsigned long valve_delay = 60; // Valve Auto Close Delay (seconds)
+unsigned long pump_delay = 60;  // Pump Auto Close Delay (seconds)
 
-//Auto Watering
-bool auto_watering = false;            //自动灌溉控制
-unsigned long watering_interval = 720; //灌溉间隔(分钟)
-bool moisture_trigger = false;         //湿度触发器
+// Auto Watering
+bool auto_watering = false;            // Auto Watering Switch
+unsigned long watering_interval = 720; // Auto Watering Interval (minutes)
+bool moisture_trigger = false;         // Moisture Trigger State (enable/disable)
 unsigned long soil_moisture = 0;
-unsigned long soil_moisture_limit = 1023; //湿度触发阈值
-unsigned long last_watering_time = 0;     //上次触发时间
+unsigned long soil_moisture_threshold = 1023; // Threshold of soil moisture trigger (0-1023)
+unsigned long last_watering_time = 0;         // Last watering time
 
-#define PUMP_PIN D1          //抽水机
-#define VALVE_PIN D2         //电磁阀 (连接继电器的端口)
-#define SOIL_MOISTURE_PIN A0 //土壤湿度传感器
-
-bool need_save_config = false; //设置保存触发器
+bool need_save_config = false;
 
 void event(const char *payload, size_t length)
 {
   const size_t capacity = JSON_OBJECT_SIZE(8) + 150;
   DynamicJsonDocument doc(capacity);
   auto error = deserializeJson(doc, payload);
-  //Test if parsing succeeds.
+  // Test if parsing succeeds.
   if (error)
   {
     return;
@@ -87,8 +93,8 @@ void event(const char *payload, size_t length)
     valve = doc["valve"];
     if (valve)
     {
-      valve_delay_trigger = true;
-      valveMillis = millis(); //重置电磁阀关闭计时
+      valve_auto_close = true;
+      valveMillis = millis(); // Reset timer
     }
   }
   if (doc["pump"] != "null")
@@ -96,20 +102,20 @@ void event(const char *payload, size_t length)
     pump = doc["pump"];
     if (pump)
     {
-      pump_delay_trigger = true;
-      pumpMillis = millis(); //重置抽水机关闭计时
+      pump_auto_close = true;
+      pumpMillis = millis(); // Reset timer
     }
   }
 
   if (doc["auto_watering"] != "null")
   {
-    auto_watering = doc["auto_watering"]; //自动灌溉
+    auto_watering = doc["auto_watering"];
     moisture_trigger = doc["auto_watering"];
     need_save_config = true;
   }
   if (doc["moisture_trigger"] != "null")
   {
-    moisture_trigger = doc["moisture_trigger"]; //湿度触发器
+    moisture_trigger = doc["moisture_trigger"];
   }
 
   if (doc["valve_delay"] != "null")
@@ -122,9 +128,9 @@ void event(const char *payload, size_t length)
     pump_delay = doc["pump_delay"];
     need_save_config = true;
   }
-  if (doc["soil_moisture_limit"] != "null")
+  if (doc["soil_moisture_threshold"] != "null")
   {
-    soil_moisture_limit = doc["soil_moisture_limit"];
+    soil_moisture_threshold = doc["soil_moisture_threshold"];
     need_save_config = true;
   }
   if (doc["watering_interval"] != "null")
@@ -133,7 +139,10 @@ void event(const char *payload, size_t length)
     need_save_config = true;
   }
 
-  event_set();
+  digitalWrite(PUMP_PIN, pump);
+  digitalWrite(VALVE_PIN, valve);
+  data_readtime = timeClient.getEpochTime();
+
   upload(0);
   if (need_save_config)
   {
@@ -142,18 +151,11 @@ void event(const char *payload, size_t length)
   }
 }
 
-void event_set()
-{
-  digitalWrite(PUMP_PIN, pump);
-  digitalWrite(VALVE_PIN, valve);
-  data_readtime = timeClient.getEpochTime();
-}
-
 void setup_wifi()
 {
   delay(10);
   WiFi.mode(WIFI_STA);
-  //We start by connecting to a WiFi network
+  // We start by connecting to a WiFi network
   WiFi.begin(ssid, password);
   while (WiFi.waitForConnectResult() != WL_CONNECTED)
   {
@@ -162,9 +164,10 @@ void setup_wifi()
   }
 }
 
+// Read sensor data
 void read_data()
 {
-  int chk = DHT.readdht(DHT_PIN); //读取传感器数据
+  int chk = DHT.readdht(DHT_PIN);
   switch (chk)
   {
   case DHTLIB_OK:
@@ -176,8 +179,8 @@ void read_data()
     temperature = NULL;
     break;
   }
-  soil_moisture = analogRead(SOIL_MOISTURE_PIN); //土壤湿度情况
-  data_readtime = timeClient.getEpochTime();     //读取数据的时间
+  soil_moisture = analogRead(SOIL_MOISTURE_PIN);
+  data_readtime = timeClient.getEpochTime();
 }
 
 void upload(bool reset)
@@ -195,16 +198,16 @@ void upload(bool reset)
   payload += "," + String(watering_interval);
   payload += "," + String(valve_delay);
   payload += "," + String(pump_delay);
-  payload += "," + String(soil_moisture_limit);
+  payload += "," + String(soil_moisture_threshold);
   payload += "\"}";
 
   char msg[200];
   payload.toCharArray(msg, 200);
 
-  webSocket.emit("devicedata", msg);
+  socketIO.emit("devicedata", msg);
 
   if (reset)
-    lastMillis = millis(); //重置上传计时
+    lastMillis = millis(); // Reset the upload data timer
 }
 
 bool load_config()
@@ -217,12 +220,12 @@ bool load_config()
 
   size_t size = configFile.size();
 
-  //Allocate a buffer to store contents of the file.
+  // Allocate a buffer to store contents of the file.
   std::unique_ptr<char[]> buf(new char[size]);
 
-  //We don't use String here because ArduinoJson library requires the input
-  //buffer to be mutable. If you don't use ArduinoJson, you may as well
-  //use configFile.readString instead.
+  // We don't use String here because ArduinoJson library requires the input
+  // buffer to be mutable. If you don't use ArduinoJson, you may as well
+  // use configFile.readString instead.
   configFile.readBytes(buf.get(), size);
 
   const size_t capacity = JSON_OBJECT_SIZE(6) + 150;
@@ -234,10 +237,10 @@ bool load_config()
     return false;
   }
 
-  // 读取配置---------------
+  // Read Config-----------
   valve_delay = doc["valve_delay"];
   pump_delay = doc["pump_delay"];
-  soil_moisture_limit = doc["soil_moisture_limit"];
+  soil_moisture_threshold = doc["soil_moisture_threshold"];
   watering_interval = doc["watering_interval"];
   auto_watering = doc["auto_watering"];
   last_watering_time = doc["last_watering_time"];
@@ -251,10 +254,10 @@ bool save_config()
   const size_t capacity = JSON_OBJECT_SIZE(6);
   DynamicJsonDocument doc(capacity);
 
-  // 保存配置---------------
+  // Save Config------------
   doc["valve_delay"] = valve_delay;
   doc["pump_delay"] = pump_delay;
-  doc["soil_moisture_limit"] = soil_moisture_limit;
+  doc["soil_moisture_threshold"] = soil_moisture_threshold;
   doc["watering_interval"] = watering_interval;
   doc["auto_watering"] = auto_watering;
   doc["last_watering_time"] = last_watering_time;
@@ -270,11 +273,11 @@ bool save_config()
   return true;
 }
 
+// Watchdog
 void ISRwatchdog()
 {
-  //看门狗
   watchdogCount++;
-  if (watchdogCount > 10) //超过十秒无响应则重置单片机
+  if (watchdogCount > 10) // Not Responding for 10 seconds, it will reset the board.
   {
     ESP.reset();
   }
@@ -282,91 +285,97 @@ void ISRwatchdog()
 
 void setup()
 {
-  // Serial.begin(115200);
+#ifdef ENABLE_DEBUG
+  Serial.begin(115200);
+#endif
 
   pinMode(PUMP_PIN, OUTPUT);
   pinMode(VALVE_PIN, OUTPUT);
   digitalWrite(PUMP_PIN, LOW);
-  digitalWrite(VALVE_PIN, LOW); //初始为关闭状态
+  digitalWrite(VALVE_PIN, LOW); //Off
 
-  SPIFFS.begin(); //配置FS
+  SPIFFS.begin(); //FS
   if (!load_config())
-    save_config(); //读取配置，否则保存默认配置
+    save_config(); // Read config, or save default settings.
 
-  setup_wifi(); //配置WIFI
+  setup_wifi(); // Setup Wi-Fi
 
-  timeClient.begin(); //NTC服务启动
+  timeClient.begin(); // Start NTC service
 
-  //OTA设置
+  // OTA
   ArduinoOTA.setPort(8266);
   ArduinoOTA.setHostname(device_name);
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    watchdogCount = 1; //Feed dog while doing update
+    DEBUG_PRINTLN((float)progress / total * 100);
+    watchdogCount = 1; // Feed dog while doing update
   });
   ArduinoOTA.begin();
 
-  //WebSocket设置
-  webSocket.beginwebsocket(server_url, server_port, "/socket.io/?transport=websocket");
-  webSocket.setAuthorization(admin_name, admin_password);
+  // WebSocket
+  socketIO.beginwebsocket(server_url, server_port);
+  socketIO.setAuthorization(admin_name, admin_password);
 
   char buf[16];
   itoa(device_id, buf, 10);
-  webSocket.on(buf, event);
+  socketIO.on(buf, event);
 
-  //Watchdog
+  // Watchdog
   secondTick.attach(1, ISRwatchdog);
 }
 
 void loop()
 {
-  watchdogCount = 1; //Feed dog
+  watchdogCount = 1; // Feed dog
 
-  ArduinoOTA.handle(); //OTA
-  timeClient.update(); //更新NTP时间
-  webSocket.loop();    //Websocket loop
+  ArduinoOTA.handle(); // OTA
+  timeClient.update(); // NTP
+  socketIO.loop();     // Websocket
 
-  //每10秒上传一次数据
+  // Upload data every 10 seconds
   if (millis() - lastMillis > 10000)
   {
     read_data();
     upload(1);
   }
 
-  //自动灌溉间隔
+  // Enable moisture trigger
   if (auto_watering && !moisture_trigger && timeClient.getEpochTime() - last_watering_time > 60 * watering_interval)
   {
     moisture_trigger = true;
     upload(0);
   }
 
-  //根据土壤湿度自动打开电磁阀
-  if (auto_watering && moisture_trigger && soil_moisture > soil_moisture_limit)
+  // Open valve when soil moisture passes threshold
+  if (auto_watering && moisture_trigger && soil_moisture > soil_moisture_threshold)
   {
-    moisture_trigger = false;
+    moisture_trigger = false; // Reset trigger state
 
-    last_watering_time = timeClient.getEpochTime(); //重置上次灌溉时间
-    save_config();
+    last_watering_time = timeClient.getEpochTime(); // Reset last watering time
+    if (save_config())
+    {
+      DEBUG_PRINTLN("Save config failed!")
+    };
 
     valve = true;
     digitalWrite(VALVE_PIN, valve);
-    valve_delay_trigger = true;
-    valveMillis = millis(); //重置电磁阀关闭计时
+    valve_auto_close = true;
+    valveMillis = millis(); // Reset valve timer
     upload(0);
   }
 
-  //延时关闭电磁阀
-  if (valve_delay_trigger && millis() - valveMillis > 1000 * valve_delay)
+  // Close valve after certain delay
+  if (valve_auto_close && millis() - valveMillis > 1000 * valve_delay)
   {
-    valve_delay_trigger = false;
+    valve_auto_close = false;
     valve = false;
     digitalWrite(VALVE_PIN, valve);
     upload(0);
   }
 
-  //延时关闭抽水机
-  if (pump_delay_trigger && millis() - pumpMillis > 1000 * pump_delay)
+  // Close pump after certain delay
+  if (pump_auto_close && millis() - pumpMillis > 1000 * pump_delay)
   {
-    pump_delay_trigger = false;
+    pump_auto_close = false;
     pump = false;
     digitalWrite(PUMP_PIN, pump);
     upload(0);
